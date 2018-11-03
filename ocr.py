41d5dcb53eb006ede5bca084e906ecbd12a598f5
AndresJulian <andresitc@gmail.com>
2018-11-03 02:01:43 -0500

    get first data

--- ocr.py
+++ ocr.py
@@ -1,3 +1,23 @@
 from PIL import Image
 import pytesseract
-print(pytesseract.image_to_string(Image.open('img1.png')))
+import os
+clear = lambda: os.system('cls' if os.name=='nt' else 'clear')
+clear()
+contenido = pytesseract.image_to_string(Image.open('img2.png'))
+# print contenido.encode('utf8')
+# print contenido.splitlines()
+InfoFactura = {}
+
+c = 0
+detalle=0
+tokens=['TOTAL','FECHA','IVA','IMPUESTO']
+for i in contenido.splitlines():
+    c=c+1
+    if c==1: InfoFactura['Razon social'] = i.encode('utf8')
+    for j in tokens:
+        if j in i.upper(): InfoFactura[j] = i.encode('utf8')
+    if 'CANT' in i.upper(): detalle=1
+    if 'TOTAL' in i.upper() and not detalle: detalle=0
+    if detalle: 
+        InfoFactura['Detalle'+ str(c)] = i.encode('utf8')
+print InfoFactura
